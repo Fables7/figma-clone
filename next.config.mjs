9be6bb6d54a229f02ab3ed3bcb61a4c8b2326851
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-02-19 21:01:30 +0000

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => ({
-    "utf-8-validate": "commonjs utf-8-validate",
-    bufferutil: "commonjs bufferutil",
-    canvas: "commonjs canvas",
-  }),
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
@@ -20,6 +24,9 @@ const nextConfig = {
   ) => {
     config.externals.push({ canvas: "commonjs canvas" });
     return config;
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
